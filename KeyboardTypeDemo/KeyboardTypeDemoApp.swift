de44dec028a467955100344eaffad4a16e716a61
Eden <edendudoo@gmail.com>
2021-08-27 09:25:58 +0800

    Initial Commit

---
+++ KeyboardTypeDemo/KeyboardTypeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardTypeDemoApp.swift
+//  KeyboardTypeDemo
+//
+//  Created by Eden on 2021/8/27.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardTypeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
